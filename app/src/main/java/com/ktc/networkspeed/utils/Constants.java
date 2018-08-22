47397eba623a6bf699654b2cdacb56ef7cbb8623
fengjw <fengjwxx@163.com>
2018-08-22 11:16:59 +0800

    recently code

---
+++ app/src/main/java/com/ktc/networkspeed/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.ktc.networkspeed.utils;
+
+public class Constants {
+
+    public static final String URL_CONFIG = "http://www.speedtest.net/speedtest-config.php";
+    public static final String URL_SERVER = "http://www.speedtest.net/speedtest-servers-static.php";
+
+}
